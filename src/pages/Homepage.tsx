ee856b744bfd6ac41b218ba536f5bee93f225560
Niveydhithaa Ramamoorthi <niveydhithaa.ramamoorthi@techmobius.com>
2022-10-22 16:30:05 +0530

    change tab name

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -40,7 +40,7 @@ export default function NavTabs() {
       <Navbar></Navbar>
       <Box sx={{ width: '80%', alignItems: "center"}} mt={4} mb={4} ml={10} >
         <Tabs value={tabIndex} onChange={handleChange} aria-label="nav tabs example">
-          <LinkTab label="Home" href="/dashboard" />
+          <LinkTab label="NICE Guidelines" href="/dashboard" />
           <LinkTab label="Patients" href="/patientslist" />
         </Tabs>
       </Box>
