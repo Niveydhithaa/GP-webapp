ef18c9ad630c06d830855da69834ce2eac810c28
Niveydhithaa Ramamoorthi <niveydhithaa.ramamoorthi@techmobius.com>
2022-10-25 17:05:46 +0530

    add reusable "new patient" form

---
+++ src/components/hooks/PatientDialog.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import {
+    Box,
+    TextField,
+    Table,
+    TableHead,
+    TableRow,
+    TableBody,
+    ToggleButtonGroup,
+    ToggleButton,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+        border: 0,
+    },
+}));
+export default function PatientDialog() {
+    const [gender, setGender] = useState("");
+    const [value, setValue] = useState<Dayjs | null>(
+        dayjs('2022-10-21T19:00:00'),
+    );
+    const handleGender = (
+        event: React.MouseEvent<HTMLElement>,
+        newGender: string
+      ) => {
+        setGender(newGender);
+    };
+    const handleChangeDate = (newValue: Dayjs | null) => {
+        setValue(newValue);
+    };
+    return (
+        <Box>
+            <Box>
+                <br></br>
+                <Box>
+                    <TextField
+                        label="Name"
+                        id="input_name"
+                        required
+                        style={{ minWidth: "350px" }}
+                    />
+                </Box>
+                <br></br>
+                <Box>
+                    <TextField
+                        label="Surname"
+                        id="input_surname"
+                        required
+                        style={{ minWidth: "350px" }}
+                    />
+                </Box>
+                <br></br>
+                <Box>
+                    <TextField
+                        label="Age"
+                        id="age"
+                        required
+                        style={{ minWidth: "350px", marginBottom: "12px" }}
+                    />
+                </Box>
+                <br></br>
+                <Box>
+                    <Box>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={gender}
+                            exclusive
+                            onChange={handleGender}
+                            aria-label="Platform"
+                        >
+                            <ToggleButton value="male">Male</ToggleButton>
+                            <ToggleButton value="female">Female</ToggleButton>
+                        </ToggleButtonGroup>
+                    </Box>
+                </Box>
+                <Box mt={2}>
+                    <TextField
+                        label="Mobile"
+                        id="mobile_num"
+                        style={{ minWidth: "350px", marginBottom: "12px" }}
+                    />
+                </Box>
+            </Box>
+        </Box>
+    )
+}
