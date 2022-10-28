484885daed58b7c1e45e959f4b34e8b236c95638
Niveydhithaa Ramamoorthi <niveydhithaa.ramamoorthi@techmobius.com>
2022-10-28 22:14:14 +0530

    add components  and comment accordion dependency

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,26 @@ import AccordionExample from "components/AccordionExample";
 import Patients from "pages/patients/Patients"
 import PatientProfile from "pages/patients/PatientProfile"
 import Home from "pages/Homepage"
+import GPProfile from "pages/GPProfile"
 import CustomBreadCrumb from "components/CustomBreadCrumb"
 import TwoStepBreadCrumb from "components/TwoStepBreadCrumb"
+import PrivateRoutesDashboard from './hooks/PrivateRouteDashboard'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+      
         <Route path="/dashboard_v2" element={<DashboardV2 />} />
-        <Route path="/accordion_reusable" element={<AccordionExample />} />
+        {/* <Route path="/accordion_reusable" element={<AccordionExample selectedSymp = {"selected"}/>} /> */}
         <Route path="/patientslist" element={<Patients />} />
         <Route path="/patientprofile" element={<PatientProfile />} />
         <Route path="/home" element={<Home />} />
         <Route path="/breads" element={<CustomBreadCrumb />} />
         <Route path="/twostepbread" element={<TwoStepBreadCrumb />} />
+        <Route path="/gpprofile" element={<GPProfile />} />
       </Routes>
     </BrowserRouter>
   );
