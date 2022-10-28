17a7c1be74be45d56c862b08634199927c7b134a
Niveydhithaa Ramamoorthi <niveydhithaa.ramamoorthi@techmobius.com>
2022-10-28 22:12:27 +0530

    add GP profile

---
+++ src/pages/GPProfile.tsx
@@ -0,0 +1,14 @@
+import Navbar from "components/Navbar";
+import {
+    Box
+} from "@mui/material";
+export default function GPProfile() {
+    return (
+        <div>
+            <Box>
+                <Navbar></Navbar>
+                <Box></Box>
+            </Box>
+        </div>
+    )
+}
