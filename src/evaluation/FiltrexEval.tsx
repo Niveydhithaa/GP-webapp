022f7b8490484cac9bbecf6ccc3b0b6832d4657f
Niveydhithaa Ramamoorthi <niveydhithaa.ramamoorthi@techmobius.com>
2022-11-29 22:46:44 +0530

    screen #2 storage and ui set (almost) except del

--- src/evaluation/FiltrexEval.tsx
+++ src/evaluation/FiltrexEval.tsx
@@ -4,20 +4,49 @@ import {
     Button,
     ToggleButton,
     ToggleButtonGroup,
-    Typography
+    Typography, 
+    TextField
 } from "@mui/material"
-import {useState, useEffect} from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 import siteJson from "data/sites_master_mod.json"
 import { isUndefined } from 'util';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import RenderQuestions from 'evaluation/FiltrexEvalPartTwo';
 
+enum ActionKind {
+    STATE = "state",
+    UPDATE = "update"
+}
+interface ActionProp {
+    type: ActionKind
+    payload: { title: string, value: boolean, question: string }
+    index?: number
+}
 interface Props {
     smoker: boolean | undefined
     asbestos: boolean | undefined
+    site: number | undefined
+    inputFields: any
 }
 
-export default function FiltrexEval({smoker, asbestos} : Props) {
+export const reducer = (state: any, action: ActionProp) => {
+    switch (action.type) {
+        case ActionKind.STATE: {
+            debugger;
+            console.log(action)
+            return { ...state, name: [...state.name, action.payload] };
+        }
+        case ActionKind.UPDATE: {
+            if (action.index !== undefined)
+                state["name"][action.index]["value"] = action.payload.value
+            // return {...state, name: [...state.name, action.payload]};
+            return { ...state };
+        }
+        default:
+            return state;
+    }
+};
+export default function FiltrexEval({ smoker, asbestos, site, inputFields, ...props }: Props) {
     const navigate = useNavigate()
     //#region state variables
     const [xray, setXray] = useState<number>();
@@ -26,10 +55,31 @@ export default function FiltrexEval({smoker, asbestos} : Props) {
     const [immediate, setImmediate] = useState<boolean>(false)
     //#endregion
     const [nextScreen, setNextScreen] = useState<boolean>(false)
+    let initialState = { name: [] };
+    const [inputFieldsScreenTwo, dispatch] = useReducer(reducer, initialState)
     useEffect(() => {
         // Run! Like go get some data from an API.
         setNextScreen(false)
-      }, []);
+        renderQuestionsToggle()
+    }, []);
+    const handleAddMoreFields = useCallback((state_name: string, state_value: boolean, question: string) => {
+        console.log("to add to list")
+        //debugger;
+        dispatch({
+            type: ActionKind.STATE,
+            payload: { title: state_name, value: state_value, question: question }
+        });
+    }, [site]);
+
+    const handleUpdateValueField = (index: number, state_name: string, state_value: boolean, question: string) => {
+        console.log("update value : index: " + index + " value: " + state_value)
+        dispatch({
+            type: ActionKind.UPDATE,
+            payload: { title: state_name, value: state_value, question: question },
+            index: index
+        });
+        console.log(inputFieldsScreenTwo.name)
+    };
     const clickHandler = () => {
         //#region Sample Expression Evaluation
         // Input from user (e.g. search filter)
@@ -47,57 +97,110 @@ export default function FiltrexEval({smoker, asbestos} : Props) {
         // console.log(a1)
 
         var screen2_expr2 = siteJson[0].screens[1].condition?.at(0)
-        console.log(typeof(screen2_expr2))
-        if(screen2_expr2!=undefined && !Array.isArray(screen2_expr2))
-        {
+        console.log(typeof (screen2_expr2))
+        if (screen2_expr2 != undefined && !Array.isArray(screen2_expr2)) {
             //xrayfindings=0 : normal
             //xrayfindings=1 : abnormal
             var screen2_filter2 = compileExpression(screen2_expr2)
             console.log(xray)
             console.log(unhaemo)
-            
-            var a2 = screen2_filter2({xray_findings: xray, unexplained_heamoptysis: unhaemo})
+
+            var a2 = screen2_filter2({ xray_findings: xray, unexplained_heamoptysis: unhaemo })
             console.log(a2)
-            if(a2)
-            {
+            if (a2) {
                 setImmediate(true)
                 setFurther(false)
             }
-            else if(!a2)
-            {
+            else if (!a2) {
                 setFurther(true)
                 setImmediate(false)
             }
         }
-        
+
     }
     const clickFurther = () => {
         // navigate("/filtrex/screen3")
         setNextScreen(true)
     }
+    const renderQuestionsToggle = () => {
+        console.log("driver function enter")
+        //make the inputFields epmty here. for each site it has to be made empty
+        let questionsList: any[] = [];
+        if (site != undefined) {
+            console.log(site)
+            let totalDict = siteJson[site - 1].screens[1].values
+            let keys = Object.keys(totalDict)
+            let values = Object.values(totalDict)
+            console.log(" Initial state of input field:  " + JSON.stringify(initialState))
+            for (var i in keys) {
+                console.log(i)
+                console.log(values[i])
+                console.log(keys[i])
+                console.log(totalDict.hasOwnProperty(keys[i]))
+                // #infinite loop problem
+                handleAddMoreFields(keys[i], true, values[i].message)
+
+                console.log(JSON.stringify(initialState))
+            }
+            console.log(" Input field after loop: " + JSON.stringify(inputFieldsScreenTwo))
+            console.log("driver function exit")
+        }
+        return questionsList;
+    }
     return (
         <Box>
-            <Typography>{siteJson[0].screens[1].display_name}</Typography>
+
+            {
+                (site !== undefined) &&
+                <Box>
+                    <Typography>{siteJson[site - 1].screens[1].display_name}</Typography>
+                    {inputFieldsScreenTwo.name.map((item: any, index: number) => {
+                        return (
+                            <Box key={index} className="Wrapper">
+                                {/* {console.log(item.name.title)} */}
+                                <Typography>{item.question} - {item.title}</Typography>
+
+                                {
+                                    (item.title != "gender" && item.title != "age") &&
+                                    <Box>
+                                        <ToggleButtonGroup
+                                            color="primary"
+                                            exclusive
+                                            value={item.value}
+                                            // onInput={}
+                                            // onClick={(e: React.MouseEvent<HTMLElement>, newValue: boolean) => handleUpdateValueField(index, item.title, newValue, item.question)}
+                                            onChange={(e: React.MouseEvent<HTMLElement>, newValue: boolean) => {
+                                                handleUpdateValueField(index, item.title, newValue, item.question)
+                                                item.value = newValue
+                                            }}
+                                            aria-label="Platform"
+                                        >
+                                            <ToggleButton value={true} id="toggle_symptom">Yes</ToggleButton>
+                                            <ToggleButton value={false}>No</ToggleButton>
+                                        </ToggleButtonGroup>
+                                    </Box>
+                                }
+                                {
+                                    (item.title == "gender" || item.title == "age")
+                                    &&
+                                    <Box>
+                                        <TextField label={item.title} disabled={true}>Autofilled</TextField>
+                                    </Box>
+                                }
+                            </Box>
+                        )
+
+                    })
+                    }
+                </Box>
+            }
             <Box>
-                {/* <Typography>XRay findings abnormal?</Typography> */}
-                <Typography>{siteJson[0].screens[1].values['xray_findings']?.display_name}</Typography>
-                <Typography fontSize={10}>Choose 'No' For normal ; 'Yes' for Abnormal findings ; 'Not available' if so</Typography>
-                <ToggleButtonGroup
-                    color="primary"
-                    value={xray}
-                    exclusive
-                    onChange={(e: any, newValue: number ) => setXray(newValue)}
-                    aria-label="Platform"
-                    >
-                    <ToggleButton value={1} id="toggle_symptom">Yes (Lung cancer)</ToggleButton>
-                    <ToggleButton value={2} id="toggle_symptom">Yes (Mesathelioma)</ToggleButton>
-                    <ToggleButton value={0}>No</ToggleButton>
-                    <ToggleButton value={3}>Not available</ToggleButton>
-                </ToggleButtonGroup>
+
             </Box>
+
+            {/*}
             <Box>
                 <Typography>{siteJson[0].screens[1].values['unexplained_heamoptysis']?.display_name}</Typography>
-                {/* <Typography>Unexplained Haemopthysis</Typography> */}
                 <ToggleButtonGroup
                     color="primary"
                     value={unhaemo}
@@ -109,15 +212,16 @@ export default function FiltrexEval({smoker, asbestos} : Props) {
                     <ToggleButton value={0}>No</ToggleButton>
                 </ToggleButtonGroup>
             </Box>
+             */}
             <Button onClick={clickHandler}>Get results</Button>
             <Box>
-            <Button disabled={!furtherInvest} color="secondary" variant="contained" onClick={clickFurther}>{siteJson[0].screens[1].termination_button_text?.at(1)}</Button>
-            <Button disabled={!immediate} color="error" variant="contained">{siteJson[0].screens[1].termination_button_text?.at(0)}</Button>
+                <Button disabled={!furtherInvest} color="secondary" variant="contained" onClick={clickFurther}>{siteJson[0].screens[1].termination_button_text?.at(1)}</Button>
+                <Button disabled={!immediate} color="error" variant="contained">{siteJson[0].screens[1].termination_button_text?.at(0)}</Button>
             </Box>
             {/* section for further offer screen */}
             {
-                nextScreen && 
-                <RenderQuestions smoker={smoker} asbestos={asbestos}/>
+                nextScreen &&
+                <RenderQuestions smoker={smoker} asbestos={asbestos} />
             }
         </Box>
     )
